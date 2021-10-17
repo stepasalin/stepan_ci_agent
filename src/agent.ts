5fe6e3db319087b1dd5ad59b5e3ce7b68345b7ca
Stepan Salin <ssalin@artec-group.com>
2021-10-17 20:21:08 +0300

    now able to add agents

--- src/agent.ts
+++ src/agent.ts
@@ -1,33 +1,34 @@
-// import { createReadStream, readFile } from 'fs-extra';
-import { AGENT_NAME } from './config';
-import { makeServerRequest } from './util/serverRequest';
+import { AGENT_NAME, SERVER_HOST, SERVER_PORT } from './config';
 import { AgentInfoManager } from './util/AgentInfoManager';
 import { logger } from './util/logger';
-// import { AGENT_NAME } from './config';
-// import { executeShellCommand } from './util/exec';
-// import { logger } from './util/logger';
+const request = require('request');
 
-// async function sendLog(): Promise<void> {}
-
-async function registerAtServer(): Promise<String> {
-  const options = {
-    path: 'add-agent',
-    method: 'POST',
-  };
-
-  return new Promise<String>(async (resolve, _reject) => {
-    const postData = { name: AGENT_NAME };
-    const serverResponse = await makeServerRequest(options, postData);
-    resolve(serverResponse.agent._id);
+function getNewAgentId(): Promise<String> {
+  return new Promise<String>((resolve, _reject) => {
+    request.post(
+      {
+        url: `http://${SERVER_HOST}:${SERVER_PORT}/add-agent`,
+        json: { name: AGENT_NAME },
+      },
+      function (err: any, httpResponse: any, body: any) {
+        logger.info('Sending request to server');
+        logger.info(`Err ${err}`);
+        logger.info(`Http Response ${httpResponse}`);
+        logger.info(`Body ${body}`);
+        resolve(body);
+      }
+    );
   });
 }
 
 async function agent(): Promise<void> {
   const infoManager = await AgentInfoManager.create();
   const infoAtStartup = await infoManager.getInfo();
-  logger.info(`agent ${AGENT_NAME} has started with info ${infoAtStartup}`);
+  logger.info(
+    `agent ${AGENT_NAME} has started with info ${JSON.stringify(infoAtStartup)}`
+  );
   if (infoAtStartup == null) {
-    const thisAgentId = await registerAtServer();
+    const thisAgentId = await getNewAgentId();
     const newAgentInfo = {
       ...AgentInfoManager.DEFAULT_INFO,
       ...{ id: thisAgentId },
