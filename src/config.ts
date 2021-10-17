2af149cb32640cb29c1520d4bb5ee95ab0d259f9
Stepan Salin <ssalin@artec-group.com>
2021-10-17 17:07:51 +0300

    add server url to config

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ export const AGENT_NAME = mustExist(
   process.env.AGENT_NAME,
   'AGENT_NAME is not defined'
 );
+export const SERVER_URL = process.env.SERVER_URL || 'http://localhost:4000';
 export const ENV = mustExist(process.env.NODE_ENV, 'NODE_ENV is not defined');
 export const LOG_DIR = '/tmp/stepan/log';
 export const TEMPLATE_PATH = join(__dirname, 'client/index.html');
