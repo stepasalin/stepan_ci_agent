6cf975919b6ae89989cb5973f3670302ad31eae3
Stepan Salin <ssalin@artec-group.com>
2021-11-01 22:04:49 +0300

    utils for calculatig log diff

--- src/util/exec.ts
+++ src/util/exec.ts
@@ -1,5 +1,7 @@
 import * as childProcess from 'child_process';
 import { logger as defaultLogger } from './logger';
+import * as fs from 'fs';
+const path = require('path');
 
 const logger = defaultLogger.child({ name: 'execute' });
 
@@ -16,4 +18,39 @@ export function executeShellCommand(cmd: String, logPath: String) {
       reject(error);
     });
   });
+}
+
+function previouslySentLogPath(logPath: string) {
+  return `${logPath}.previous`;
+}
+
+function stringDiff(str1: string, str2: string) {
+  let diff = '';
+  str2.split('').forEach(function (val, i) {
+    if (val != str1.charAt(i)) diff += val;
+  });
+  return diff;
+}
+
+async function readFile(path: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+export async function newLog(logPath: string) {
+  const currentLog = await readFile(logPath);
+  const previousLogPath = previouslySentLogPath(logPath);
+
+  if (!path.existsSync(previousLogPath)) {
+    return currentLog;
+  }
+
+  const previousLog = await readFile(previousLogPath);
+  return stringDiff(previousLog, currentLog);
 }
