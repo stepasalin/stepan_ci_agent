cbf157ea6e680be3be2c9ea69e871af71c773e69
Stepan Salin <ssalin@artec-group.com>
2021-11-05 20:50:52 +0300

    do not use old new log calculation method

--- src/busyAgent.ts
+++ src/busyAgent.ts
@@ -1,11 +1,11 @@
-import { newLog } from './util/exec';
+// import { newLog } from './util/exec';
 import { AgentInfoManager, AgentInfo } from './util/AgentInfoManager';
 
 export async function busyAgent(
   infoManager: AgentInfoManager,
   agentInfo: AgentInfo
 ): Promise<void> {
-  const { logPath } = agentInfo;
-  const newLogEntry = await newLog(logPath);
-  console.log(`lets do something ${newLogEntry} !`);
+  // const { logPath } = agentInfo;
+  // const newLogEntry = await newLog(logPath);
+  // console.log(`lets do something ${newLogEntry} !`);
 }
